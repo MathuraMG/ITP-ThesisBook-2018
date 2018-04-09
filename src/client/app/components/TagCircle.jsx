7b84722525fa623ae8685262ff8408f73feaaeee
mathuramg <mmg9291@gmail.com>
2018-04-09 15:12:18 -0400

    clean lint

--- src/client/app/components/TagCircle.jsx
+++ src/client/app/components/TagCircle.jsx
@@ -1,81 +1,23 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-import { withFauxDOM } from 'react-faux-dom';
 
 const d3 = require('d3');
 const axios = require('axios');
 const ReactFauxDOM = require('react-faux-dom');
 
-const testVar = 'hello';
 let svg;
 class TagCircle extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dummy: 0,
-      mouseOver: false,
-      svg: []
+      mouseOver: false
     };
   }
-  componentDidUpdate() {
-    console.log('updates');
-  }
-  getPairedProjects(tag) {
-    axios.get(`/api/pair/${tag}`)
-      .then((res) => {
-        this.props.setSelectedTags(res.data);
-        this.props.getTagProjects(tag);
-      });
-  }
 
-
-  packageImports(nodes) {
-    let map = {},
-      imports = [];
-
-    // Compute a map from name to node.
-    nodes.forEach((d) => {
-      map[d.data.name] = d;
-    });
-
-    // For each import, construct a link from the source to target node.
-    nodes.forEach((d) => {
-      if (d.data.imports) {
-        d.data.imports.forEach((i) => {
-          imports.push(map[d.data.name].path(map[i]));
-        });
-      }
-    });
-
-    return imports;
-  }
-
-  packageHierarchy(classes) {
-    const map = {};
-
-    function find(name, data) {
-      let node = map[name],
-        i;
-      if (!node) {
-        node = map[name] = data || { name, children: [] };
-        if (name.length) {
-          node.parent = find(name.substring(0, i = name.lastIndexOf('.')));
-          node.parent.children.push(node);
-          node.key = name.substring(i + 1);
-        }
-      }
-      return node;
-    }
-
-    classes.forEach((d) => {
-      find(d.name, d);
-    });
-
-    return d3.hierarchy(map['']);
-  }
   componentDidMount() {
-    let diameter = 600,
-      radius = diameter / 2,
-      innerRadius = radius - 120;
+    const diameter = 600;
+    const radius = diameter / 2;
+    const innerRadius = radius - 120;
 
     const cluster = d3.cluster()
       .size([360, innerRadius]);
@@ -85,14 +27,13 @@ class TagCircle extends React.Component {
       .radius(d => d.y)
       .angle(d => d.x / 180 * Math.PI);
 
-    const self = this;
     svg = d3.select(ReactFauxDOM.createElement('svg'))
       .attr('width', diameter)
       .attr('height', diameter)
       .attr('class', 'tagCircle__container');
 
-    let link = svg.append('svg').selectAll('.link'),
-      node = svg.append('svg').selectAll('.node');
+    let link = svg.append('svg').selectAll('.link');
+    let node = svg.append('svg').selectAll('.node');
 
     d3.json('flare.json', (error, classes) => {
       if (error) throw error;
@@ -105,7 +46,7 @@ class TagCircle extends React.Component {
       link = link
         .data(this.packageImports(root.leaves()))
         .enter().append('path')
-        .each((d) => { d.source = d[0], d.target = d[d.length - 1]; })
+        .each((d) => { d.source = d[0]; d.target = d[d.length - 1]; })
         .attr('class', this.state.mouseOver ? 'link' : 'link')
         .attr('transform', `translate(${radius},${radius})`)
         .attr('d', line);
@@ -133,6 +74,7 @@ class TagCircle extends React.Component {
             if (n.data.name === d.data.name) {
               return true;
             }
+            return false;
           });
           this.props.setSelectedTag(d.data.name);
           this.getPairedProjects(d.data.name);
@@ -140,6 +82,60 @@ class TagCircle extends React.Component {
 
       this.setState({ mouseOver: !this.state.mouseOver });
     });
+  }
+
+  getPairedProjects(tag) {
+    axios.get(`/api/pair/${tag}`)
+      .then((res) => {
+        this.props.setSelectedTags(res.data);
+        this.props.getTagProjects(tag);
+      });
+  }
+
+
+  packageImports(nodes) {
+    const map = {};
+    const imports = [];
+
+    // Compute a map from name to node.
+    nodes.forEach((d) => {
+      map[d.data.name] = d;
+    });
+
+    // For each import, construct a link from the source to target node.
+    nodes.forEach((d) => {
+      if (d.data.imports) {
+        d.data.imports.forEach((i) => {
+          imports.push(map[d.data.name].path(map[i]));
+        });
+      }
+    });
+
+    return imports;
+  }
+
+  packageHierarchy(classes) {
+    const map = {};
+
+    function find(name, data) {
+      let node = map[name];
+      let i;
+      if (!node) {
+        node = map[name] = data || { name, children: [] };
+        if (name.length) {
+          node.parent = find(name.substring(0, i = name.lastIndexOf('.')));
+          node.parent.children.push(node);
+          node.key = name.substring(i + 1);
+        }
+      }
+      return node;
+    }
+
+    classes.forEach((d) => {
+      find(d.name, d);
+    });
+
+    return d3.hierarchy(map['']);
   }
 
   render() {
@@ -162,5 +158,12 @@ class TagCircle extends React.Component {
     );
   }
 }
+
+TagCircle.propTypes = {
+  getTagProjects: PropTypes.func.isRequired,
+  setSelectedTag: PropTypes.func.isRequired,
+  setIsTagCircleOpen: PropTypes.func.isRequired,
+  setSelectedTags: PropTypes.func.isRequired,
+};
 
 export default TagCircle;
--- src/client/app/reducers/project.jsx
+++ src/client/app/reducers/project.jsx
@@ -2,187 +2,186 @@ import * as ActionTypes from '../constants.jsx';
 
 const initialState = {
   isTagCircleOpen: false,
-  selectedProject:{},
-  selectedProjects:[],
-  selectedStudent:'',
-  selectedTag:'',
-  selectedTags:[],
+  selectedProject: {},
+  selectedProjects: [],
+  selectedStudent: '',
+  selectedTag: '',
+  selectedTags: [],
   showSingleProject: false,
   tags: [
-    "Art",
-    "Education",
-    "Performance",
-    "Installation",
-    "Storytelling",
-    "Social Good",
-    "VR",
-    "Behavior",
-    "Product-Tool",
-    "Machine Learning",
-    "UX-UI",
-    "Music-Sound",
-    "Algorithms",
-    "Politics",
-    "Privacy",
-    "Mobile Apps",
-    "Wearables",
-    "Speculative Design",
-    "Data Viz",
-    "Games",
-    "Internet of Things",
-    "Networking"
+    'Art',
+    'Education',
+    'Performance',
+    'Installation',
+    'Storytelling',
+    'Social Good',
+    'VR',
+    'Behavior',
+    'Product-Tool',
+    'Machine Learning',
+    'UX-UI',
+    'Music-Sound',
+    'Algorithms',
+    'Politics',
+    'Privacy',
+    'Mobile Apps',
+    'Wearables',
+    'Speculative Design',
+    'Data Viz',
+    'Games',
+    'Internet of Things',
+    'Networking'
   ],
-  students: ["aaron-parsekian",
-  "aarón-montoya-moraga",
-  "adi-wiedersheim-sendagorta",
-  "angela-perrone",
-  "anne-k-goodfriend",
-  "chelsea-jane-pfohl",
-  "chino-kim",
-  "christina-b-choi",
-  "corbin-y-ordel",
-  "cristina-cannella",
-  "dana-abrassart",
-  "daniel-silber-baker",
-  "danielle-i-butler",
-  "daveed-r.-lockard",
-  "dhruv-damle",
-  "dhruv-r-mehrotra",
-  "doo-yon-kim-(don)",
-  "ella-dagan-pelled",
-  "emmanuel-y-klein",
-  "esther-hersh",
-  "eunjee-kim",
-  "eve-j-weinberg",
-  "fengyi-franklin-zhu",
-  "fengyuan-zhu",
-  "francesca-rodriguez-sawaya",
-  "gal-nissim",
-  "gao-yuan",
-  "gustavo-eduardo-de-campos-abbott",
-  "hao-tseng",
-  "ian-c-gibson",
-  "isabel-m-donlon",
-  "ishaan-m-chaudhary",
-  "jamal-m-combs",
-  "jamie-charry",
-  "jamie-ruddy",
-  "jared-friedman",
-  "jason-beck",
-  "jed-d-watson",
-  "jennifer-kagan",
-  "jesse-a-horwitz",
-  "jessica-l-scott-dutcher",
-  "jiaqi-guo",
-  "joakim-gregersen-quach",
-  "jonathan-james-gallagher",
-  "jordan-frand",
-  "juan-jose-egusquiza",
-  "kc-lathrop",
-  "katie-temrowski",
-  "kevin-stirnweis",
-  "kritchaya-twitchsri",
-  "kylin-chen",
-  "leon-eckert",
-  "leslie-ruckman",
-  "lindsey-f-johnson",
-  "lisa-jamhoury",
-  "lisa-song",
-  "marcela-nowak",
-  "mathura-m-govindarajan",
-  "melanie-hoff",
-  "michelle-hessel-alves",
-  "muqing-niu",
-  "nai-chen-yang",
-  "naoki-ishizuka",
-  "nicola-carpeggiani",
-  "nicole-he",
-  "nikita-r-huggins",
-  "nilomee-c-jesrani",
-  "olivia-cueva",
-  "ondina-frate",
-  "osama-m-sehgol",
-  "paula-ceballos-delgado",
-  "peiyu-luo",
-  "philip-j-donaldson",
-  "ravyn-l-whitley",
-  "rebecca-ricks",
-  "regina-vetka",
-  "renata-de-carvalho-gaui",
-  "ruta-kruliauskaite",
-  "satbir-samantha-multani",
-  "serena-m-parr",
-  "shir-david",
-  "sisa-bueno",
-  "skylar-jessen",
-  "song-eun-we",
-  "soyeon-chung",
-  "viniyata-pany",
-  "wangshu-sun",
-  "william-erwin-quest-kennelly",
-  "wipawe-sirikolkarn",
-  "xingjian-an",
-  "xiqiao-zhou",
-  "xiwei-huang",
-  "xuhui-xu",
-  "yan-zhao",
-  "yao",
-  "ying-he",
-  "yiting-liu",
-  "yuchi-ma",
-  "yue-hu",
-  "yue-zhang",
-  "yuli-cai",
-  "yun-li",
-  "zhuoxi",
-  "zoe-bachman",
-  "rebecca-(marks)-leopold"]
+  students: ['aaron-parsekian',
+    'aarón-montoya-moraga',
+    'adi-wiedersheim-sendagorta',
+    'angela-perrone',
+    'anne-k-goodfriend',
+    'chelsea-jane-pfohl',
+    'chino-kim',
+    'christina-b-choi',
+    'corbin-y-ordel',
+    'cristina-cannella',
+    'dana-abrassart',
+    'daniel-silber-baker',
+    'danielle-i-butler',
+    'daveed-r.-lockard',
+    'dhruv-damle',
+    'dhruv-r-mehrotra',
+    'doo-yon-kim-(don)',
+    'ella-dagan-pelled',
+    'emmanuel-y-klein',
+    'esther-hersh',
+    'eunjee-kim',
+    'eve-j-weinberg',
+    'fengyi-franklin-zhu',
+    'fengyuan-zhu',
+    'francesca-rodriguez-sawaya',
+    'gal-nissim',
+    'gao-yuan',
+    'gustavo-eduardo-de-campos-abbott',
+    'hao-tseng',
+    'ian-c-gibson',
+    'isabel-m-donlon',
+    'ishaan-m-chaudhary',
+    'jamal-m-combs',
+    'jamie-charry',
+    'jamie-ruddy',
+    'jared-friedman',
+    'jason-beck',
+    'jed-d-watson',
+    'jennifer-kagan',
+    'jesse-a-horwitz',
+    'jessica-l-scott-dutcher',
+    'jiaqi-guo',
+    'joakim-gregersen-quach',
+    'jonathan-james-gallagher',
+    'jordan-frand',
+    'juan-jose-egusquiza',
+    'kc-lathrop',
+    'katie-temrowski',
+    'kevin-stirnweis',
+    'kritchaya-twitchsri',
+    'kylin-chen',
+    'leon-eckert',
+    'leslie-ruckman',
+    'lindsey-f-johnson',
+    'lisa-jamhoury',
+    'lisa-song',
+    'marcela-nowak',
+    'mathura-m-govindarajan',
+    'melanie-hoff',
+    'michelle-hessel-alves',
+    'muqing-niu',
+    'nai-chen-yang',
+    'naoki-ishizuka',
+    'nicola-carpeggiani',
+    'nicole-he',
+    'nikita-r-huggins',
+    'nilomee-c-jesrani',
+    'olivia-cueva',
+    'ondina-frate',
+    'osama-m-sehgol',
+    'paula-ceballos-delgado',
+    'peiyu-luo',
+    'philip-j-donaldson',
+    'ravyn-l-whitley',
+    'rebecca-ricks',
+    'regina-vetka',
+    'renata-de-carvalho-gaui',
+    'ruta-kruliauskaite',
+    'satbir-samantha-multani',
+    'serena-m-parr',
+    'shir-david',
+    'sisa-bueno',
+    'skylar-jessen',
+    'song-eun-we',
+    'soyeon-chung',
+    'viniyata-pany',
+    'wangshu-sun',
+    'william-erwin-quest-kennelly',
+    'wipawe-sirikolkarn',
+    'xingjian-an',
+    'xiqiao-zhou',
+    'xiwei-huang',
+    'xuhui-xu',
+    'yan-zhao',
+    'yao',
+    'ying-he',
+    'yiting-liu',
+    'yuchi-ma',
+    'yue-hu',
+    'yue-zhang',
+    'yuli-cai',
+    'yun-li',
+    'zhuoxi',
+    'zoe-bachman',
+    'rebecca-(marks)-leopold']
 };
 
 const project = (state = initialState, action) => {
   switch (action.type) {
-
     case ActionTypes.SET_IS_TAG_CIRCLE_OPEN:
-      {
-        return Object.assign({},state,{
-          isTagCircleOpen: action.value
-        })
-      }
+    {
+      return Object.assign({}, state, {
+        isTagCircleOpen: action.value
+      });
+    }
     case ActionTypes.SET_SELECTED_PROJECT:
-      {
-        return Object.assign({}, state, {
-          selectedProject: action.value,
-          showSingleProject: true
-        });
-      }
+    {
+      return Object.assign({}, state, {
+        selectedProject: action.value,
+        showSingleProject: true
+      });
+    }
 
     case ActionTypes.SET_SELECTED_PROJECTS:
-      {
-        return Object.assign({}, state, {
-          selectedProjects: action.value,
-          showSingleProject: false
-        });
-      }
+    {
+      return Object.assign({}, state, {
+        selectedProjects: action.value,
+        showSingleProject: false
+      });
+    }
     case ActionTypes.SET_SELECTED_TAG:
-      {
-        return Object.assign({}, state, {
-          selectedTag: action.value
-        });
-      }
+    {
+      return Object.assign({}, state, {
+        selectedTag: action.value
+      });
+    }
 
     case ActionTypes.SET_SELECTED_TAGS:
-      {
-        return Object.assign({}, state, {
-          selectedTags: action.value
-        });
-      }
+    {
+      return Object.assign({}, state, {
+        selectedTags: action.value
+      });
+    }
 
     case ActionTypes.SET_SELECTED_STUDENT:
-      {
-        return Object.assign({}, state, {
-          selectedStudent: action.value
-        });
-      }
+    {
+      return Object.assign({}, state, {
+        selectedStudent: action.value
+      });
+    }
 
     default:
       return state;
