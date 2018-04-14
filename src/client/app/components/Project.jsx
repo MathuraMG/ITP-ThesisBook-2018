6ba32daf43178a411a909fdac5a8bb26e99ce079
mathuramg <mmg9291@gmail.com>
2018-04-14 11:50:20 -0400

    add image carousel

--- src/client/app/components/Project.jsx
+++ src/client/app/components/Project.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Carousel } from 'react-responsive-carousel';
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 class Project extends React.Component {
   renderImages(images) {
     const array = [];
-    images.forEach((image) => {
-      array.push(<img
-        src={image.src}
-      />);
+    images.forEach((image, index) => {
+      array.push(<div key={index}>
+        <img
+          src={image.src}
+        />
+        <p className="legend">Legend</p>
+      </div>);
     });
 
     return array;
   }
   render() {
     const renderArray = this.renderImages(this.props.selectedProject.slide_show);
+    console.log(renderArray);
     return (
       <section>
         <h1>{this.props.selectedProject.project_title}</h1>
@@ -21,7 +27,9 @@ class Project extends React.Component {
         <p>{this.props.selectedProject.project_question}</p>
         <p>{this.props.selectedProject.short_description}</p>
         <p>{this.props.selectedProject.description}</p>
-        {renderArray}
+        <Carousel>
+          {renderArray}
+        </Carousel>
       </section>
     );
   }
